538c90678dd118b8ea5d9907767f72666ca5ebc4
Angel-GL-GL <angelel.garciaia@gmail.com>
2024-07-12 21:42:35 -0600

    update

---
+++ MTS_APIs/mts_users/src/main/java/com/services/api/mts_users/models/Users.java
@@ -0,0 +1,11 @@
+package com.services.api.mts_users.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+}
