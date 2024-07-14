5c4b754266373c93fefa69490c73659be366d603
Angel-GL-GL <angelel.garciaia@gmail.com>
2024-07-14 02:31:31 -0600

    Update Helper.java

--- MTS_APIs/mts_users/src/main/java/com/services/api/mts_users/models/Helper.java
+++ MTS_APIs/mts_users/src/main/java/com/services/api/mts_users/models/Helper.java
@@ -3,6 +3,7 @@ package com.services.api.mts_users.models;
 import lombok.Getter;
 import lombok.Setter;
 
+//Clase solamente para ayuda en los json
 public class Helper {
     @Getter @Setter
     private String password;
