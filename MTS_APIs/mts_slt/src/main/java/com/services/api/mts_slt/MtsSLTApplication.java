5357f80deeb85657ed34713ae2bfba3a3a2e3bf9
Angel-GL-GL <angelel.garciaia@gmail.com>
2024-07-30 00:15:11 -0600

    MTSSLT

---
+++ MTS_APIs/mts_slt/src/main/java/com/services/api/mts_slt/MtsSLTApplication.java
@@ -0,0 +1,13 @@
+package com.services.api.mts_slt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsSLTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsSLTApplication.class, args);
+	}
+
+}
---
+++ MTS_APIs/mts_slt/src/test/java/com/services/api/mts_slt/MtsSLTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.api.mts_slt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsSLTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
