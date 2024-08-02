c622ce05a2efa9761afe61d9005645e965fabef3
Angel-GL-GL <angelel.garciaia@gmail.com>
2024-08-01 21:35:11 -0600

    Update Transports_Controllers.java

--- MTS_APIs/mts_slt/src/main/java/com/services/api/mts_slt/controllers/Transports_Controllers.java
+++ MTS_APIs/mts_slt/src/main/java/com/services/api/mts_slt/controllers/Transports_Controllers.java
@@ -5,9 +5,15 @@ import com.services.api.mts_slt.services.Transports_Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 public class Transports_Controllers {
     @Autowired
     private Transports_Service service;
+
+    @RequestMapping(value = "api/transports", method = RequestMethod.GET)
+    private ResponseEntity<List<Transports>> getTransports(){
+        return ResponseEntity.ok(service.getAllTransports());
+    }
 }
