cf740ed8390b75c2733c9b8efe85220d7c56f5af
Angel-GL-GL <angelel.garciaia@gmail.com>
2024-08-02 02:11:30 -0600

    Update Stations_Controllers.java

--- MTS_APIs/mts_slt/src/main/java/com/services/api/mts_slt/controllers/Stations_Controllers.java
+++ MTS_APIs/mts_slt/src/main/java/com/services/api/mts_slt/controllers/Stations_Controllers.java
@@ -1,17 +1,23 @@
 package com.services.api.mts_slt.controllers;
 
+import com.services.api.mts_slt.models.RSMs;
+import com.services.api.mts_slt.models.ScheduleHelper;
 import com.services.api.mts_slt.models.Stations;
+import com.services.api.mts_slt.services.RSMs_Service;
 import com.services.api.mts_slt.services.Stations_Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Stations_Controllers {
     @Autowired
     private Stations_Service service;
+    @Autowired
+    private RSMs_Service rsm_service;
 
     @RequestMapping(value = "api/stations", method = RequestMethod.GET)
     private ResponseEntity<List<Stations>> getAllStations(){
@@ -26,6 +32,21 @@ public class Stations_Controllers {
     @RequestMapping(value = "api/lines/{line}/stations", method = RequestMethod.GET)
     private ResponseEntity<List<Stations>> getStationsByLine(@PathVariable Integer line){
         return ResponseEntity.ok(service.getAllStationsByLine(line));
+    }
+
+    @RequestMapping(value = "api/routes/{route}/stations", method = RequestMethod.GET)
+    private ResponseEntity<List<Stations>> getStationsByRoute(@PathVariable Integer route){
+        List<RSMs> matches = rsm_service.getAllRSMsByRoute(route);
+
+        if(matches.isEmpty()) return ResponseEntity.ok(new ArrayList<Stations>());
+
+        ArrayList<Stations> res = new ArrayList<>(matches.size());
+
+        for(RSMs rsm: matches){
+            res.add(service.getStation(rsm.getId()));
+        }
+
+        return ResponseEntity.ok(res);
     }
 
     @RequestMapping(value = "api/stations/incidents/{in}", method = RequestMethod.GET)
