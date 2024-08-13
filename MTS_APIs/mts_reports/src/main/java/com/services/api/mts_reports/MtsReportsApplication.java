965b3d2bdf467a10100ccc22902db9822e1f1eba
Angel-GL-GL <angelel.garciaia@gmail.com>
2024-08-12 19:39:42 -0600

    Reports

---
+++ MTS_APIs/mts_reports/src/main/java/com/services/api/mts_reports/MtsReportsApplication.java
@@ -0,0 +1,13 @@
+package com.services.api.mts_reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsReportsApplication.class, args);
+	}
+
+}
---
+++ MTS_APIs/mts_reports/src/test/java/com/services/api/mts_reports/MtsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.api.mts_reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
