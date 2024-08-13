5f1737323816f7981c991a8515ac037718cb3245
Angel-GL-GL <angelel.garciaia@gmail.com>
2024-08-12 19:44:48 -0600

    Update MtsReportsApplication.java

--- MTS_APIs/mts_reports/src/main/java/com/services/api/mts_reports/MtsReportsApplication.java
+++ MTS_APIs/mts_reports/src/main/java/com/services/api/mts_reports/MtsReportsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MtsReportsApplication {
+public class MtsReportsApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(MtsReportsApplication.class, args);
