1a3d59b932136c012b0c7034ac58f08c62290839
Angel-GL-GL <angelel.garciaia@gmail.com>
2024-09-04 18:19:51 -0600

    New API

---
+++ MTS_APIs/mts_evidences/src/main/java/com/services/api/mts_evidences/MtsEvidencesApplication.java
@@ -0,0 +1,13 @@
+package com.services.api.mts_evidences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsEvidencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsEvidencesApplication.class, args);
+	}
+
+}
---
+++ MTS_APIs/mts_evidences/src/test/java/com/services/api/mts_evidences/MtsEvidencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.api.mts_evidences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsEvidencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
